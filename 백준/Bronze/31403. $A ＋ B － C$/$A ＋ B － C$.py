478bbd6028d7e56107c9bb4b01da05ad69521ba1
playdourbest <103979879+playdourbest@users.noreply.github.com>
2024-06-14 17:11:55 +0900

    [Bronze IV] Title: $A + B - C$, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.py
@@ -0,0 +1,13 @@
+# a = int(input())
+# b = int(input())
+# c = int(input())
+
+# print(a+b-c)
+# num1 = str(a)+str(b)
+# print(int(num1)-c)
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+print(a+b-c, int(f'{a}{b}')-c)
