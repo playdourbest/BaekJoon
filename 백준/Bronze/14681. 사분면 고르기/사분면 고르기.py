605cc379f2f29f83e48a83df7f4ee3dbb5bc72bb
playdourbest <103979879+playdourbest@users.noreply.github.com>
2024-06-14 09:45:55 +0900

    [Bronze V] Title: 사분면 고르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,28 @@
+x = input()
+y = input()
+
+try:
+    x = int(x)
+    y = int(y)
+
+    if x > 0 and y > 0:
+        print(1)
+    elif x < 0 and y > 0:
+        print(2)
+    elif x < 0 and y < 0:
+        print(3)
+    else:
+        print(4)
+
+except:
+    x = float(x)
+    y = float(y)
+
+    if x > 0 and y > 0:
+        print(1)
+    elif x < 0 and y > 0:
+        print(2)
+    elif x < 0 and y < 0:
+        print(3)
+    else:
+        print(4)
