1b3b80abe2edea35584912e3e34411b9ff8d6bd9
playdourbest <103979879+playdourbest@users.noreply.github.com>
2024-06-14 09:16:07 +0900

    [Bronze V] Title: 사칙연산, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,17 @@
+a, b = input().split()
+try:
+    a = int(a)
+    b = int(b)
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(int(a/b))
+    print(a%b)
+except:
+    a = float(a)
+    b = float(b)
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(int(a/b))
+    print(a%b)
