56251f0866887bad4c0b1dc54d75bc67c2d0710f
playdourbest <103979879+playdourbest@users.noreply.github.com>
2024-06-18 15:29:50 +0900

    Update ACM 호텔.py

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -11,3 +11,7 @@ for i in range(t):
             k += j
             room_num.append(k)
     print(room_num[n-1])
+
+# for _ in range(int(input())):
+#  a,b,c=map(int,input().split())
+#  print(((c-1)%a+1)*100+(c-1)//a+1)
